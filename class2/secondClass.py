3540acb1345fbc30450a396689b8e0b9bb3060df
Amirshah98760 <amirshah98760@gmail.com>
2026-05-19 12:08:19 +0500

    Second class Started

---
+++ class2/secondClass.py
@@ -0,0 +1,7 @@
+# String in python 
+# We Use single cote , double cote and tripple cote for Creating String
+
+str1 = 'Ammi Shah'
+str1 = "My Name is Amir Shah"
+str1 = """I am a full Stack Developer"""
+
